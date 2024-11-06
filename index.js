73b981c72e386ad84d22ff451362789b4a6f7a12
farhanzaky94 <farhanzaky94@gmail.com>
2024-11-06 17:36:16 +0700

    add button in html and add function in js

---
+++ index.js
@@ -0,0 +1,13 @@
+const display = document.getElementById(`display`);
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function ClearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
